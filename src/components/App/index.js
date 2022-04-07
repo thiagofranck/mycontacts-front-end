a757a7cb68149cd02d96e53aad0014058f493cfe
Thiago Henrique <thiagohenrique776@gmail.com>
2022-04-06 21:16:28 -0300

    Routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,27 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 
 import Header from '../Header';
-import ContactsList from '../ContactsList';
 
 import { Container } from './styles';
+import Routes from '../../routes';
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
 
-      <Container>
-        <Header />
-        <ContactsList />
-      </Container>
+        <Container>
+          <Header />
+          <Routes />
+        </Container>
 
-    </ThemeProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
