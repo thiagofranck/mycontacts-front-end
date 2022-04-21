32a68bbb154b3bb689ab9d7a929131ec3c1a6893
Thiago Henrique <thiagohenrique776@gmail.com>
2022-04-21 16:15:13 -0300

    Component PageHeader, Input e Select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,3 +1,24 @@
+import PageHeader from '../../components/PageHeader';
+
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
 export default function Home() {
-  return 'NewContact';
+  return (
+    <>
+      <PageHeader
+        title="Novo contato"
+      />
+
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
+  );
 }
