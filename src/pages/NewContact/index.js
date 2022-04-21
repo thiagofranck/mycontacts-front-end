a0d67c37fd95fb4a24e73bb421cbb42c13cbde10
Thiago Henrique <thiagohenrique776@gmail.com>
2022-04-21 16:26:31 -0300

    Component Button

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -2,6 +2,7 @@ import PageHeader from '../../components/PageHeader';
 
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import Button from '../../components/Button';
 
 export default function Home() {
   return (
@@ -19,6 +20,13 @@ export default function Home() {
         <option value="123">Instagram</option>
         <option value="123">Instagram</option>
       </Select>
+
+      <Button type="button">
+        Salvar alterações
+      </Button>
+      <Button type="button" disabled>
+        Salvar alterações
+      </Button>
     </>
   );
 }
