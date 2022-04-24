49ef8f178471f66041a92368542bea8c1a8afa5b
Thiago Henrique <thiagohenrique776@gmail.com>
2022-04-24 19:03:55 -0300

    Criação de service de contatos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,7 +10,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
 import Loader from '../../components/Loader';
-import delay from '../../utils/delay';
+import ContatctsService from '../../services/ContatctsService';
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
@@ -27,13 +27,9 @@ export default function Home() {
       try {
         setIsLoading(true);
 
-        const response = await fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`);
+        const contactsList = await ContatctsService.listContacts(orderBy);
 
-        await delay(500);
-
-        const json = await response.json();
-        setContacts(json);
-        setIsLoading(false);
+        setContacts(contactsList);
       } catch (error) {
         console.log('Error', error);
       } finally {
